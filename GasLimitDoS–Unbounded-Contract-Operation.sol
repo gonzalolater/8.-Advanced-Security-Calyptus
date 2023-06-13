53f6b4566208dfe2a4cdfce920978f0e7f1b4b7a
Gonzalo Daniel Aguilar <42863568+gonzalolater@users.noreply.github.com>
2023-06-13 11:46:17 -0300

    Update GasLimitDoS–Unbounded-Contract-Operation.sol

--- GasLimitDoS–Unbounded-Contract-Operation.sol
+++ GasLimitDoS–Unbounded-Contract-Operation.sol
@@ -14,3 +14,23 @@
 //       }
 //       nextPayeeIndex = i;
 // }  
+contract Auction {
+   address frontRunner;
+   uint256 highestBid;
+   mapping(address => uint) public balances;
+  function bid() public payable {
+     require(msg.value > highestBid, "Need to be higher than highest bid");
+     // update previous bidder info
+     balances[frontRunner] += highestBid;
+     // update new bidder
+     frontRunner = msg.sender;
+     highestBid = msg.value;
+  }
+  function withdraw() public nonReentrant {
+       require(msg.sender != frontRunner, "Current frontRunner cannot withdraw");
+       uint amount = balances[msg.sender];
+       balances[msg.sender] = 0;
+       (bool sent, ) = msg.sender.call{value: amount}("");
+       require(sent, "Failed to send Ether");
+   }
+}
