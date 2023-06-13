9b45883ceea5d25019eff14a5e7cf2ed97690777
Gonzalo Daniel Aguilar <42863568+gonzalolater@users.noreply.github.com>
2023-06-13 11:37:12 -0300

    Create Auction.sol

---
+++ Auction.sol
@@ -0,0 +1,12 @@
+contract Auction {
+   address frontRunner;
+   uint256 highestBid;
+ 
+   function bid() public payable {
+       require(msg.value > highestBid, "Need to be higher than highest bid");
+       // Refund the old leader, if it fails then revert  
+       require(payable(frontRunner).send(highestBid), "Failed to send Ether");
+       frontRunner = msg.sender;
+       highestBid = msg.value;
+   }
+}
